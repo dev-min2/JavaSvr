a77736754b4bf16befb7f34af77f6ca9c9f96c97
mky <c11286@naver.com>
2023-08-24 23:08:46 +0900

    링버퍼 수정

--- ServerUtil/src/SockNet/RingBuffer.java
+++ ServerUtil/src/SockNet/RingBuffer.java
@@ -4,13 +4,8 @@ import java.nio.ByteBuffer;
 
 public class RingBuffer {	
 	private static final int DEFAULT_BUFFER_SIZE = 1024 * 10; 
-	
-	private int readPos = 0;
-	private int writePos = 0;
-	private int offset = 0; // 현재 버퍼안의 데이터 시작위치
-	private byte[] buffer;
+
 	private ByteBuffer byteBuffer;
-	private ByteBuffer btMark = null;
 	private int remainLen = 0;
 	
 	public RingBuffer()
@@ -22,37 +17,16 @@ public class RingBuffer {
 	public RingBuffer(final int bufferSize)
 	{
 		byteBuffer = ByteBuffer.allocate(bufferSize);
-		//buffer = new byte[bufferSize];
-	}
-	
-	public int getCurrentBufferDataSize()
-	{
-		return writePos - readPos;
-	}
-	
-	public int getBufferFreeSize()
-	{
-		return buffer.length - writePos;
 	}
 	
 	public ByteBuffer getBuffer()
 	{
 		return byteBuffer.slice();
 	}
-	
-	public boolean writeBuffer(byte[] dataBuffer)
-	{
-		byteBuffer.put(dataBuffer);
-		
-		//System.arraycopy(dataBuffer, 0, buffer, offset + writePos, dataBuffer.length);
-		//writePos += dataBuffer.length;
-		return true;
-	}
-	
-	public boolean readBuffer(int readDataSize)
+
+	public void readBuffer(int readDataSize)
 	{
 		byteBuffer.get(new byte[readDataSize]);
-		return true;
 	}
 	
 	public void clean()
